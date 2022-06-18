9a31c9ddf7e383bc929b2edf234747c141f37c63
Matthew Mage <sciguymjm@gmail.com>
2022-06-18 15:36:38 -0700

    Use a custom fork of boxcars for testing deserialization

--- src/actor_handlers/utils.rs
+++ src/actor_handlers/utils.rs
@@ -2,7 +2,7 @@ use crate::frame_parser::Actor;
 use boxcars::attributes::{RemoteId, UniqueId};
 use boxcars::Attribute;
 use log::warn;
-use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use serde::{Serialize, Deserialize};
 use std::collections::{hash_map::DefaultHasher, HashMap};
 use std::fmt;
 use std::hash::{Hash, Hasher};
@@ -157,7 +157,7 @@ fn rotator_to_quat(pitch: f32, yaw: f32, roll: f32) -> (f32, f32, f32, f32) {
   (w / norm, x / norm, y / norm, z / norm)
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct WrappedUniqueId(UniqueId);
 
 impl WrappedUniqueId {
@@ -229,26 +229,26 @@ impl fmt::Display for WrappedUniqueId {
   }
 }
 
-impl Serialize for WrappedUniqueId {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-      S: Serializer,
-  {
-    serializer.serialize_str(&self.to_string())
-  }
-}
-
-impl<'de: 'a, 'a> Deserialize<'de> for WrappedUniqueId {
-  fn deserialize<D>(_deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-  {
-    Ok(WrappedUniqueId {
-      0: UniqueId {
-        system_id: 0,
-        remote_id: RemoteId::Epic(String::from("")),
-        local_id: 0
-      },
-    })
-  }
-}
+// impl Serialize for WrappedUniqueId {
+//   fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//       S: Serializer,
+//   {
+//     serializer.serialize_str(&self.to_string())
+//   }
+// }
+//
+// impl<'de: 'a, 'a> Deserialize<'de> for WrappedUniqueId {
+//   fn deserialize<D>(_deserializer: D) -> Result<Self, D::Error>
+//     where
+//         D: Deserializer<'de>,
+//   {
+//     Ok(WrappedUniqueId {
+//       0: UniqueId {
+//         system_id: 0,
+//         remote_id: RemoteId::Epic(String::from("")),
+//         local_id: 0
+//       },
+//     })
+//   }
+// }
